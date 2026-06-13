822814b32deeb3fd20c3c6fd81da1ccdfad6adcd
Theophilus (Theo) Abner <theophilusabner0204@yahoo.com>
2026-06-13 00:53:59 -0400

    Initial commit for Bryant's Son Home Improvement website

---
+++ Pages/Privacy.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Bryant_s_Son_Home_Improvement__LLC.Pages;
+
+public class PrivacyModel : PageModel
+{
+    public void OnGet()
+    {
+    }
+}
+
